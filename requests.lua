0fefc8be7f4e304904a15c5af3f4c30746e201d5
root <myownroc@163.com>
2019-12-18 14:09:12 +0800

    change index

--- requests.lua
+++ requests.lua
@@ -1,8 +1,8 @@
 local route = ""
 local header =
-"HTTP/1.1 200 OK\r\n" ..
-"Content-Length: {{ .length }}" ..
-"\r\n" ..
+"HTTP/1.1 {{ .responseCode }} {{ .responseStatus }}\r\n" ..
+"Content-Length: {{ .length }}\r\n" ..
+"{{ .addedHeader }}" ..
 "\r\n"
 local template = ""
 local html = ""
@@ -26,6 +26,9 @@ if (#route > 1) then
         end)()
         html = template
         html = string.gsub(html, "{{(%s*).dataString(%s*)}}", json)
+        header = string.gsub(header, "{{(%s*).responseCode(%s*)}}", "200")
+        header = string.gsub(header, "{{(%s*).responseStatus(%s*)}}", "OK")
+        header = string.gsub(header, "{{(%s*).addedHeader(%s*)}}", "")
     else
         local f = (function()
             local file = io.open("./echarts.min.js", "rb")
@@ -36,17 +39,43 @@ if (#route > 1) then
             return data
         end)()
         html = f
+        header = string.gsub(header, "{{(%s*).responseCode(%s*)}}", "200")
+        header = string.gsub(header, "{{(%s*).responseStatus(%s*)}}", "OK")
+        header = string.gsub(header, "{{(%s*).addedHeader(%s*)}}", "")
     end
 else
-    template = (function()
-        local file = io.open("./index.html", "rb")
-        local len = file:seek("end")
-        file:seek("set")
-        local data = file:read(len)
-        io.close(file)
-        return data
-    end)()
-    html = template
+    code = ""
+    for _, v in string.gmatch(__buffer, '( /%?code=)(%w+)') do
+        code = v
+        break
+    end
+    if (code == "") then
+        template = (function()
+            local file = io.open("./index.html", "rb")
+            local len = file:seek("end")
+            file:seek("set")
+            local data = file:read(len)
+            io.close(file)
+            return data
+        end)()
+        html = template
+        header = string.gsub(header, "{{(%s*).responseCode(%s*)}}", "200")
+        header = string.gsub(header, "{{(%s*).responseStatus(%s*)}}", "OK")
+        header = string.gsub(header, "{{(%s*).addedHeader(%s*)}}", "")
+    else
+        template = (function()
+            local file = io.open("./found.html", "rb")
+            local len = file:seek("end")
+            file:seek("set")
+            local data = file:read(len)
+            io.close(file)
+            return data
+        end)()
+        html = template
+        header = string.gsub(header, "{{(%s*).responseCode(%s*)}}", "302")
+        header = string.gsub(header, "{{(%s*).responseStatus(%s*)}}", "Found")
+        header = string.gsub(header, "{{(%s*).addedHeader(%s*)}}", "Location: /" .. code .. "\r\n")
+    end
 end
 header = string.gsub(header, "{{(%s*).length(%s*)}}", #html)
 __sendHttpResponse(__conn, header .. html)
